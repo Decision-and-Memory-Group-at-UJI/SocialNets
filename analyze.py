34f41ea38f1bb089bc0235795ba7e56c99ad9bd5
lemiceterieux <aghouse1@gmail.com>
2023-11-20 23:02:57 +0100

    Add LME modeling  in analysis

--- analyze.py
+++ analyze.py
@@ -8,11 +8,15 @@ corrs = []
 trueRank = np.array([[1,2,4,0,3],[2,1,0,4,3]])
 rankAcc = []
 RankConf = []
+cCoef = []
+PD = []
 for ii,part in enumerate(sorted(glob.glob("data/*.csv"))):
     X = pd.read_csv(part)
     Runs = []
     RunAcc = []
     RunRankConf = []
+    cCoefR = []
+    PDR = []
     # for runs
     for k in range(2):
         if 'Stimuli' in X:
@@ -32,20 +36,27 @@ for ii,part in enumerate(sorted(glob.glob("data/*.csv"))):
         choice = np.array(json.loads(X.Choice[k]))
         if len(choice) < 40:
             continue
-        BWgroup = np.array(json.loads(X.BWgroup[k]))
-        Soc = np.array(json.loads(X.Query[k]))
+        BWgroup = (json.loads(X.BWgroup[k]))
+        Soc = (json.loads(X.Query[k]))
         performance = (corrChoice == choice)
-        wiGaSoc = performance[np.where(np.logical_and(BWgroup == 0,Soc == 0))[0]].mean()
-        wiGaLoc = performance[np.where(np.logical_and(BWgroup == 0,Soc == 1))[0]].mean()
-        bwGaSoc = performance[np.where(np.logical_and(BWgroup == 1,Soc == 0))[0]].mean()
-        bwGaLoc = performance[np.where(np.logical_and(BWgroup == 1,Soc == 1))[0]].mean()
+        wiGaSoc = performance[np.where(np.logical_and(np.array(BWgroup) == 0,np.array(Soc) == 0))[0]].mean()
+        wiGaLoc = performance[np.where(np.logical_and(np.array(BWgroup) == 0,np.array(Soc) == 1))[0]].mean()
+        bwGaSoc = performance[np.where(np.logical_and(np.array(BWgroup) == 1,np.array(Soc) == 0))[0]].mean()
+        bwGaLoc = performance[np.where(np.logical_and(np.array(BWgroup) == 1,np.array(Soc) == 1))[0]].mean()
+
+        Soc = list(map(lambda x: "Social" if x else "NonSocial",Soc))
+        BWgroup = list(map(lambda x: "Between" if x else "Within",BWgroup))
+        PDR += [[performance,Soc,BWgroup,np.repeat(ii,40),np.repeat(k,40)]]
         RT0 = np.array(json.loads(X.retrievalRT[k]))
         Conf0 = np.array(json.loads(X.retrievalConf[k]),dtype=float)
         RT0 = np.delete(RT0,np.where(Conf0!=Conf0)[0])
         Conf0 = np.delete(Conf0,np.where(Conf0!=Conf0)[0])
         corrs += [[RT0[:len(Conf0)],Conf0,performance[:len(Conf0)]]]
         print("Corr Run {0:d}".format(k),np.corrcoef(corrs[-1][0],corrs[-1][1])[0,1])
+        cCoefR += [np.corrcoef(corrs[-1][0],corrs[-1][1])[0,1]]
         Runs += [[wiGaSoc,wiGaLoc,bwGaSoc,bwGaLoc]]
+    PD += [PDR]
+    cCoef += [cCoefR]
     RankConf += [RunRankConf]
     AllAcc += [RunAcc]
     All += [Runs]
@@ -106,3 +117,19 @@ plt.xticks([1,2],['Correct','Incorrect'])
 plt.title("T test {0:.3f}, p value {1:.3f}".format(*stats.ttest_ind(C[1][C[2]==1],C[1][C[2]!=1])))
 plt.ylabel("Confidence")
 plt.savefig("PerfConf")
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import statsmodels.formula.api as smf
+PDP = [[pd.DataFrame({"performance":PD[i][j][0].astype(int),"Social":PD[i][j][1],"Episode":PD[i][j][2],"Participant":PD[i][j][3],"Run":PD[i][j][4],"Ranking":AllCompAcc[i,j].squeeze().repeat(40)}) for j in range(2)] for i in range(6)]
+PDPCat = pd.concat([PDP[i][j] for i in range(6) for j in range(2)])
+PDPCat["RunPart"] = PDPCat["Participant"].astype(str)+PDPCat["Run"].astype(str)
+fit = smf.mixedlm('performance ~ Social+Episode +Ranking + Episode:Social +Social:Ranking + Episode:Ranking + Episode:Social:Ranking',data=PDPCat,groups=PDPCat['RunPart']).fit(reml=False)
+plt.figure(figsize=[10,8])
+plt.plot(fit.params.values, fit.params.keys(),"*")
+for key,up,low in zip(fit.params.keys(), fit.conf_int(0.05)[0], fit.conf_int(0.05)[1]):
+    plt.plot([low,up],[key,key])
+plt.xlabel("Coeff Value")
+plt.tight_layout()
+plt.savefig("LMEFit")
+fit.summary().tables[1].to_csv("SummaryMEff.csv")
+print(fit.summary(alpha=0.05))
